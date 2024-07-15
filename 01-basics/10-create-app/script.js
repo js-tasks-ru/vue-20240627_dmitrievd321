62b5764cb4c62b942f12c83008fadf1b4af6ca5a
Dmitry <dmitrievd321@gmail.com>
2024-07-16 02:36:19 +0300

    01-basics 10-create-app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,20 @@
 import { defineComponent, createApp } from 'vue/dist/vue.esm-bundler.js'
+
+const App = defineComponent({
+
+  name: 'App',
+
+
+  setup() {
+    const todaysDate = new Date().toLocaleDateString(navigator.language, { dateStyle: 'long' })
+
+    return {
+        todaysDate
+    }
+  },
+
+  template: `<div>Сегодня {{ todaysDate }}</div>`,
+})
+
+
+createApp(App).mount('#app')
