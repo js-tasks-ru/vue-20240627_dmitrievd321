a9a8188dc01a2d2759faf31b67295e08e25968a2
Dmitry <dmitrievd321@gmail.com>
2024-08-16 03:16:45 +0300

    02-basics-20-broken-map

--- 02-basics-2/20-broken-map/MapApp.js
+++ 02-basics-2/20-broken-map/MapApp.js
@@ -1,39 +1,67 @@
-import { defineComponent, ref, watch } from 'vue'
+import { defineComponent, ref, watch, onMounted, computed } from 'vue'
 
 export default defineComponent({
   name: 'MapApp',
 
   setup() {
+
+    // Условие задачи
+
+    //  // Реактивные переменные для хранения координат метки
+    //  let x = ref(0)
+    //  let y = ref(0)
+
+    //  /**
+    //   * Обработчик клика по карте для установки координат метки
+    //   * @param {MouseEvent} event
+    //   */
+    //  function handleClick(event) {
+    //    x = event.offsetX
+    //    y = event.offsetY
+    //  }
+
+    //  // Следим за X и Y для установки нового положения
+    //  watch([x, y], () => {
+    //    // Находим метку и изменяем её положение
+    //    const map = document.querySelector('.pin')
+    //    map.style.left = `${x}px`
+    //    map.style.top = `${y}px`
+    //  })
+
+    //  return {
+    //    handleClick,
+    //  }
+
+
+
     // Реактивные переменные для хранения координат метки
     let x = ref(0)
     let y = ref(0)
 
+
     /**
-     * Обработчик клика по карте для установки координат метки
-     * @param {MouseEvent} event
-     */
+    * Обработчик клика по карте для установки координат метки
+    * @param {MouseEvent} event
+    */
     function handleClick(event) {
-      x = event.offsetX
-      y = event.offsetY
+      x.value = event.offsetX
+      y.value = event.offsetY
     }
 
-    // Следим за X и Y для установки нового положения
-    watch([x, y], () => {
-      // Находим метку и изменяем её положение
-      const map = document.querySelector('.pin')
-      map.style.left = `${x}px`
-      map.style.top = `${y}px`
-    })
+    const movePin = computed(() => ({left: `${x.value}px`, top: `${y.value}px`}))
 
     return {
       handleClick,
+      movePin
     }
+
+
   },
 
   template: `
     <div class="map" @click="handleClick">
       <img class="map-image" src="./map.png" alt="Map" draggable="false" />
-      <span class="pin">📍</span>
+      <span class="pin" :style="movePin">📍</span>
     </div>
   `,
 })
